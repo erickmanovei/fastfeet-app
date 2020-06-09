8b2dfd7fa99678005333168baec69b59815f12a4
Érick Nilson Souza Sodré Filho <erickmanovei@hotmail.com>
2020-06-09 18:16:12 -0300

    Configuração do eslint prettier e root import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "prettier/prettier": "error",
+
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      }
+    }
+  }
 };
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+// import { Container } from './styles';
+
+const App = () => (
+  <View style={{ width: '100%', height: '100%', backgroundColor: '#0FF' }} />
+);
+
+export default App;
